9d899e11fe946b390d477d761515e29d04257860
Allester Fox <fox.axon@yandex.ru>
2017-09-29 21:45:43 +0500

    FPS counter, RGB565 color format for frame buffer (WIP)

--- HUB08-RGBLED.ino
+++ HUB08-RGBLED.ino
@@ -1,152 +1,125 @@
-
-// Led panel info
-#define LED_SIZE_X	24		// Size in pixels (Width)
-#define LED_SIZE_Y	12		// Size in pixels (Height)
-#define LED_CHIPS	6		// 6 chips per color
-#define LED_ROWS	3		// "1/3 scan". Its just 3 different lines, selected by pins A and B.
-#define LED_BLOCKS	3		// 3 "blocks" of pixels (8x12, vertical). 2 chips per block.
-#define LED_PPR		(LED_SIZE_X * LED_SIZE_Y) / LED_ROWS	// Pixels per row
-
-// Arduino pins
-#define PIN_CLK			10
-#define PIN_LATCH		9
-#define PIN_OE			11	// OE = Output enable
-
-#define PIN_DATA_A		2
-#define PIN_DATA_B		3
-
-#define PIN_LED_R		6
-#define PIN_LED_G		7
-#define PIN_LED_B		8
-
 /*
-unsigned long timerPrevious = 0;
-unsigned long timerInterval = 5000;
-uint8_t imageNum = 0;
+ Name:		HUB08_12x24_RGB.ino
+ Created:	9/29/2017 8:19:44 PM
+ Author:	Fox
+
+ HUB08 pinouts P13.33-3S-1R1G1B-24X12
+ GND	A
+ GND	B
+ GND	C
+ OE		D
+ RA1	G1
+ B1		RB1
+ GND	LAT
+ GND	CLK
 */
 
-uint8_t imageR[] = {
-	1, 0, 1, 0, 1, 0, 1, 0,   1, 0, 1, 0, 1, 0, 1, 0,   1, 0, 1, 0, 1, 0, 1, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
 
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 1, 0, 1, 0, 1, 0, 1,   0, 1, 0, 1, 0, 1, 0, 1,   0, 1, 0, 1, 0, 1, 0, 1,
+#define PANEL_SIZE_X	24
+#define PANEL_SIZE_Y	12
+#define PANEL_SCAN_N	3
 
+#define PIN_CLK		3
+#define PIN_LATCH	4
+#define PIN_OE		5
+#define PIN_A		10
+#define PIN_B		9
+//#define PIN_C		N/C
+//#define PIN_D		N/C
+#define PIN_RED		8
+#define PIN_GREEN	6
+#define PIN_BLUE	7
+
+unsigned long fps_delay = 1000;
+unsigned long fps_last = 6;
+unsigned long fps_start, fps_stop;
+unsigned char fps_count = 0;
+
+uint16_t frame_buffer[PANEL_SIZE_X * PANEL_SIZE_Y] = { 0xffff, };
+
+/*
+// Test pattern
+uint16_t frame_buffer[PANEL_SIZE_X * PANEL_SIZE_Y] = {
+	0xffff, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xffff, 0x001f,
+	0x07e0, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff,
+	0x07e0, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff,
+	0x07e0, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff,
+	
+	0x07e0, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff,
+	0x07e0, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff,
+	0x07e0, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff,
+	0x07e0, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff,
+	
+	0x07e0, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff,
+	0x07e0, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff,
+	0x07e0, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0xffff, 0x0000, 0x0000, 0xffff, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0xffff,
+	0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0xffff, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f, 0xf800, 0x001f,
 };
+*/
 
-uint8_t imageG[] = {
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 1, 1, 1, 0, 0,   0, 1, 1, 0, 0, 0, 1, 1,   0, 0, 0, 0, 1, 1, 0, 0,
-	0, 0, 1, 1, 1, 1, 1, 0,   1, 1, 1, 1, 0, 0, 0, 1,   1, 0, 0, 1, 1, 0, 0, 0,
-	0, 0, 1, 1, 0, 0, 0, 1,   1, 0, 0, 1, 1, 0, 0, 0,   1, 1, 1, 1, 0, 0, 0, 0,
-	0, 0, 1, 1, 1, 1, 0, 1,   1, 0, 0, 1, 1, 0, 0, 0,   0, 1, 1, 0, 0, 0, 0, 0,
-
-	0, 0, 1, 1, 1, 0, 0, 1,   1, 0, 0, 1, 1, 0, 0, 0,   0, 1, 1, 0, 0, 0, 0, 0,
-	0, 0, 1, 1, 0, 0, 0, 1,   1, 0, 0, 1, 1, 0, 0, 0,   1, 1, 1, 1, 0, 0, 0, 0,
-	0, 0, 1, 1, 0, 0, 0, 0,   1, 1, 1, 1, 0, 0, 0, 1,   1, 0, 0, 1, 1, 0, 0, 0,
-	0, 0, 1, 1, 0, 0, 0, 0,   0, 1, 1, 0, 0, 0, 1, 1,   0, 0, 0, 0, 1, 1, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-};
-
-uint8_t imageB[] = {
-	0, 1, 0, 1, 0, 1, 0, 1,   0, 1, 0, 1, 0, 1, 0, 1,   0, 1, 0, 1, 0, 1, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 0,   0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 1, 0, 1, 0, 1, 0,   1, 0, 1, 0, 1, 0, 1, 0,   1, 0, 1, 0, 1, 0, 1, 0,
-};
-
-//static unsigned long timeCounter = 0;
-
-void setup()
-{
+// the setup function runs once when you press reset or power the board
+void setup() {
 	Serial.begin(115200);
 
 	pinMode(PIN_CLK, OUTPUT);
 	pinMode(PIN_LATCH, OUTPUT);
 	pinMode(PIN_OE, OUTPUT);
-	pinMode(PIN_DATA_A, OUTPUT);
-	pinMode(PIN_DATA_B, OUTPUT);
-	pinMode(PIN_LED_R, OUTPUT);
-	pinMode(PIN_LED_G, OUTPUT);
-	pinMode(PIN_LED_B, OUTPUT);
+	pinMode(PIN_A, OUTPUT);
+	pinMode(PIN_B, OUTPUT);
+	pinMode(PIN_RED, OUTPUT);
+	pinMode(PIN_GREEN, OUTPUT);
+	pinMode(PIN_BLUE, OUTPUT);
 
-	digitalWrite(PIN_LED_R, LOW);
-	digitalWrite(PIN_LED_G, LOW);
-	digitalWrite(PIN_LED_B, LOW);
+	digitalWrite(PIN_RED, LOW);
+	digitalWrite(PIN_GREEN, LOW);
+	digitalWrite(PIN_BLUE, LOW);
 
-	digitalWrite(PIN_OE, HIGH);
 	digitalWrite(PIN_LATCH, LOW);
 	digitalWrite(PIN_CLK, LOW);
 
-	delay(1000);
+	analogWrite(PIN_OE, 240);	// Brightness, inverted: 255 is minimum!
 }
 
-void loop()
-{
-	/*
-	unsigned long timerCurrent = millis();
-	if (timerCurrent - timerPrevious >= timerInterval) {
-		timerPrevious = timerCurrent;
-		switch (imageNum)
-		{
-		default:
-			imageNum = 0;
-			return;
-			break;
-		}
-		
-		imageNum++;
-	}
-	*/
+// the loop function runs over and over again until power down or reset
+void loop() {
+	fps_start = millis();
 
-	for (uint8_t row = 0; row < LED_ROWS; row++)
+	for (uint8_t row = 0; row < 3; row++)
 	{
 		// Row select
-		digitalWrite(PIN_DATA_A, !!(row & B00000001));
-		digitalWrite(PIN_DATA_B, !!(row & B00000010));
+		digitalWrite(PIN_A, !!(row & B00000001));
+		digitalWrite(PIN_B, !!(row & B00000010));
 
-		for (uint8_t block = 0; block < LED_BLOCKS; block++)	// 3
+		for (uint8_t block = 0; block < 3; block++)	// 3 lines (Scans)
 		{
-			for (uint8_t rowInBlock = 0; rowInBlock < (LED_SIZE_Y / LED_ROWS); rowInBlock++)	// 2
+			for (uint8_t rowInBlock = 0; rowInBlock < (PANEL_SIZE_Y / 3); rowInBlock++)	// 6 chips
 			{
-				for (uint8_t colInBlock = 0; colInBlock < (LED_SIZE_X / LED_BLOCKS); colInBlock++)	// 8
+				for (uint8_t colInBlock = 0; colInBlock < (PANEL_SIZE_X / 3); colInBlock++)	// 8 byte/pixel per chip
 				{
-					int pos = (rowInBlock + 1) * LED_SIZE_X * LED_ROWS - row * LED_SIZE_X - LED_SIZE_X / LED_BLOCKS * block - colInBlock - 1;
-					digitalWrite(PIN_LED_R, imageR[pos]);
-					digitalWrite(PIN_LED_G, imageG[pos]);
-					digitalWrite(PIN_LED_B, imageB[pos]);
+					int pos = (rowInBlock + 1) * PANEL_SIZE_X * 3 - row * PANEL_SIZE_X - PANEL_SIZE_X / 3 * block - colInBlock - 1;
+					digitalWrite(PIN_RED, 1);
+					digitalWrite(PIN_GREEN, 1);
+					digitalWrite(PIN_BLUE, 1);
 
 					digitalWrite(PIN_CLK, HIGH);
 					digitalWrite(PIN_CLK, LOW);
 				}
 			}
 		}
-		
+
+		// update frame
 		digitalWrite(PIN_LATCH, HIGH);
 		digitalWrite(PIN_LATCH, LOW);
-		
-		// Brightness. Less = bright (PWM-Like)
-		digitalWrite(PIN_OE, LOW);
-		delayMicroseconds(100);
-		digitalWrite(PIN_OE, HIGH);
+	}
+	fps_stop = millis();
+	fps_count++;
+
+	if (millis() > fps_delay) {
+		Serial.print(fps_count);
+		Serial.print(" FPS, ");
+		Serial.print(((fps_stop - fps_start) + fps_last) / 2);
+		Serial.print(" ms\n");
+		fps_count = 0;
+		fps_delay = millis() + 1000;
 	}
 }
